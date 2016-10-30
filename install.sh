3b2f1ee1d6df33075c079aeecd7fc53ab86eacb1
tomsik68 <tomsik68@gmail.com>
2016-10-30 08:47:03 +0100

    add install script thingy
    Signed-off-by: tomsik68 <tomsik68@gmail.com>

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Running from $(pwd)
+
+ln -s $(pwd)/vim/ $HOME/.vim
+ln -s $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/bash/bashrc $HOME/.bashrc
+ln -s $(pwd)/bash/profile $HOME/.profile
