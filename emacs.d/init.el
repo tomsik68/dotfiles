4c11add3e0d365a63cbef92767f858ecb59be584
Tomas Jasek <tomsik68@gmail.com>
2017-08-01 08:06:55 +0200

    emacs: add graphviz package, change default font
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,17 @@
 (package-initialize)
 
+(load-file "~/.emacs.d/secret.el")
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(nil nil t)
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 120 :width normal)))))
