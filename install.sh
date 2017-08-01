2f4205cdb7fb06bc198c5c098f26c11ca5300a11
Tomas Jasek <tomsik68@gmail.com>
2017-08-01 08:08:59 +0200

    install: neovim configuration
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

--- install.sh
+++ install.sh
@@ -10,12 +10,14 @@ function bk_ln() {
     bk_mv $1
     ln -s $(pwd)/$2 $HOME/$1
 }
+ln -s vim nvim
 
 bk_ln .tmux.conf tmux/tmux.conf
 bk_ln .bashrc bash/bashrc
 bk_ln .profile bash/profile
 bk_ln .vim vim
 bk_mv .vimrc
+bk_ln .config/nvim nvim
 bk_ln .config/bspwm bspwm
 bk_ln .gitconfig git/gitconfig
 [ -d ~/.emacs.d ] || mkdir ~/.emacs.d
