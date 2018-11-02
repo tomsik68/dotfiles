63c5c94d4c6e08db301b078bd501ff9a2423f07d
Tomas Jasek <tomsik68@gmail.com>
2018-11-02 19:41:26 +0100

    bash: extend $PATH variable
    Signed-off-by: Tomas Jasek <tomsik68@gmail.com>

--- bash/bashrc.d/vars.sh
+++ bash/bashrc.d/vars.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 [ -d ~/.gem/ruby/ ] && export PATH=$PATH:~/.gem/ruby/2.4.0/bin
 export PATH=$PATH:~/dotfiles/scripts
+export PATH=$PATH:~/.cargo/bin
+export PATH=$PATH:~/bin/
+export PATH=$PATH:~/go/bin
+export PATH=$PATH:~/.config/composer/vendor/bin/
 export EDITOR=$(which nvim)
 which g++ && export CXX=$(which g++)
 export TERM=screen-256color
