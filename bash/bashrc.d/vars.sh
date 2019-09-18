1c2b9ab237d44764893c6c7d0f8b1d0035f2d8f7
Tomas Jasek <tomsik68@gmail.com>
2019-09-18 09:12:22 +0200

    bash: adjust path setting

--- bash/bashrc.d/vars.sh
+++ bash/bashrc.d/vars.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 [ -d ~/.gem/ruby/ ] && export PATH=$PATH:~/.gem/ruby/2.6.0/bin
+export PATH=~/bin/:$PATH
 export PATH=$PATH:~/dotfiles/scripts
 export PATH=$PATH:~/.cargo/bin
-export PATH=$PATH:~/bin/
 export PATH=$PATH:~/go/bin
 export PATH=$PATH:~/.config/composer/vendor/bin/
+export PATH=$PATH:~/.local/share/radare2/prefix/bin
 export EDITOR=$(which nvim)
 which g++ && export CXX=$(which g++)
 export TERM=screen-256color
