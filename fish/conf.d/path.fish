f7b502e2ad8749bed41eee7027727f9c66b4260d
Tomas Jasek <tomsik68@gmail.com>
2019-09-18 09:12:54 +0200

    fish: add bin folder and ruby gems to path

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -4,5 +4,7 @@ set PATH $PATH $DEVKITPRO/tools/bin
 set PATH $PATH ~/dotfiles/scripts
 set PATH $PATH ~/.cargo/bin
 set PATH $PATH ~/go/bin
+set PATH $PATH ~/bin
 set PATH $PATH ~/.config/composer/vendor/bin
 set PATH $PATH ~/.local/share/radare2/prefix/bin
+set PATH $PATH ~/.gem/ruby/2.6.0/bin/
