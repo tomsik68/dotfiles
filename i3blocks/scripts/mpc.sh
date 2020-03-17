5dc1bfb8b1ff02085564b67e5ef2a089cbcbcf42
Tomas Jasek <tomsik68@posteo.net>
2020-03-17 11:10:45 +0100

    i3blocks: allow mpd control via mpc block

--- i3blocks/scripts/mpc.sh
+++ i3blocks/scripts/mpc.sh
@@ -2,5 +2,14 @@
 
 if [ $(mpc status | wc -l) -gt 1 ]; then
   echo -n ">"
-  mpc status | head -n1
+  mpc -f %title% status
 fi
+
+case $BLOCK_BUTTON in
+  [1])
+    mpc toggle >/dev/null
+  ;;
+  [2-3])
+    mpc clear
+  ;;
+esac
