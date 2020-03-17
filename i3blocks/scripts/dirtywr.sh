51a6a18594c891fae2c5e1d73ea02680b2f0f275
Tomas Jasek <tomsik68@posteo.net>
2020-03-17 11:08:00 +0100

    i3blocks: simplify dirtywr script
    and also show it without external devices

--- i3blocks/scripts/dirtywr.sh
+++ i3blocks/scripts/dirtywr.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-
-bdcount=$(ls /sys/block | wc -l)
-
-if [ $bdcount -ge 3 ]; then
-  echo -n "wr "
-  grep Dirty /proc/meminfo | sed -e 's/Dirty:[^0-9]*\([0-9]\+\)[^0-9]*/\1/g' | numfmt --from-unit=1024 --to=iec-i
-fi
+grep "Dirty:" /proc/meminfo | column -H 1,3 -t | numfmt --from-unit=1000 --to=si --padding=7 --suffix=B
