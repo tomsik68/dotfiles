a7083b753cb33ddb8754f6e57c9836bac1140628
Tomas Jasek <tomsik68@posteo.net>
2020-03-17 11:07:27 +0100

    i3blocks: update battery script
    - do not show "Full"

--- i3blocks/scripts/battery.sh
+++ i3blocks/scripts/battery.sh
@@ -3,6 +3,7 @@
 acpi -b | \
   sed -e s'/Battery [0-9]://g' \
       -e s'/Discharging, //g' \
+      -e s'/Full, //g' \
       -e s'/Charging, /\^/g' \
       -e s'/,//g' \
       -e s'/remaining//g' \
