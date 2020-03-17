d4ab15a99327421b44499707b22eca595c5adb03
Tomas Jasek <tomsik68@posteo.net>
2020-03-17 11:08:24 +0100

    i3blocks: extend flags.sh script
    - poll connection to gateway if Internet unavailable
    - check if systemd services are ok
    - check if last backup was done within 30 days

--- i3blocks/scripts/flags.sh
+++ i3blocks/scripts/flags.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# I is for internet down
+# I is for internet down, G is for gateway unreachable
 check_online() {
   # 1. check if some interface is up
   interfaces=$(ls /sys/class/net | grep -v '^lo$')
   someup="0"
   for intf in $interfaces; do
-    if [[ "x$(cat /sys/class/net/$intf/operstate/)" == "xup" ]]; then
+    if [[ "x$(cat /sys/class/net/$intf/operstate)" == "xup" ]]; then
       someup="1"
       break
     fi
@@ -15,15 +15,59 @@ check_online() {
   # if all interfaces are down do not expect to be online.
   if [ $someup -eq 0 ]; then
     echo "1"
+    return
   fi
 
   if ping -w 1 -c 1 1.1.1.1 2>/dev/null >/dev/null; then
     echo "1"
   else
-    echo "I"
+    # check if gateway is reachable
+    if ping -w 1 -c _gateway 2>/dev/null >/dev/null; then
+      echo "I"
+    else
+      echo "G"
+    fi
   fi
 }
 
-str="$(determine_online)"
+# S is for system degraded, U is for user degraded
+check_systemd() {
+  system_status=$(systemctl is-system-running)
+  user_status=$(systemctl --user is-system-running)
+  case $system_status in
+    "running" )
+      echo "1"
+      ;;
+    "starting")
+      echo "1"
+      ;;
+    *)
+      echo "S"
+    ;;
+  esac
+  case $user_status in
+    "running" )
+      echo "1"
+      ;;
+    "starting")
+      echo "1"
+      ;;
+    *)
+      echo "S"
+    ;;
+  esac
+}
 
-echo $str | tr -d "1"
+# B is for last backup older than 30 days
+check_backup() {
+  last_backup=$(cat ~/.lastbackup)
+  now=$(date +%s)
+  diff=$(echo "($now - $last_backup)/86400" | bc)
+  if [ $diff -gt 30 ]; then
+    echo "B"
+  fi
+}
+
+str="$(check_online) $(check_systemd) $(check_backup)"
+
+echo $str | tr -d "1 "
